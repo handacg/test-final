bbc9c6b4f1da37cb227c8b7388691c1d584ff4ee
handacg <handa.cheng@gmail.com>
2018-12-05 17:58:50 +0100

    jquery attempt

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+//= require jquery
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-
 
 
 //= require assets/scripts/extensions/revolution.extension.actions.min
